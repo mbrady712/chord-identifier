703dcd7132f8ab5af1514b557b6d37cbe5cf81ff
Michael Brady <michaelbrady712@gmail.com>
2024-11-12 18:18:42 -0600

    Added styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import ChordIdentifier from './ChordIdentifier';
 function App() {
   return (
     <div className="App">
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
       <ChordIdentifier />
     </div>
   );
